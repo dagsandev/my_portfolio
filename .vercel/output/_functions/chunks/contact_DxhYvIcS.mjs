e07ef2ee7b5eb0f9b6d0f1cba14b6b816860bd86
Santiago Daglio <santidaglio@gmail.com>
2024-04-03 16:26:05 -0300

    deleted aos dependency and its implementations

---
+++ .vercel/output/_functions/chunks/contact_DxhYvIcS.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_OnH2llGP.mjs').then(n => n.c);
+
+export { page };
---
+++ .vercel/output/_functions/chunks/generic_Ba08Qpk1.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_B2crsuXg.mjs');
+
+export { page };
